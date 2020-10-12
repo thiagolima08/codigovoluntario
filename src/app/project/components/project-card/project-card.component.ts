5e2a364cd7b79585c33b5a3dcc18435a8cf1559e
Ediko Dominike <ediko.dominike@gmail.com>
2020-10-12 15:16:03 -0300

    createProjectCard

--- src/app/project/components/project-card/project-card.component.ts
+++ src/app/project/components/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-project-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-card.component.css']
 })
 export class ProjectCardComponent implements OnInit {
+
+  @Input()
+  project: Project;
 
   constructor() { }
 
